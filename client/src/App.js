44d270b5205f67406fd693d6ad069ef97387077b
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2019-01-17 11:01:01 +0530

    Reformatted code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import store from "./store";
 
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +8,7 @@ import "./App.css";
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 import Products from './components/products.component';
 import UserOrders from './components/userOrder/userOrders.component';
@@ -20,33 +20,33 @@ import Header from "./components/Header";
 
 class App extends Component {
 
-  render() {
-    return (
-     <Router>
-        <Provider store={store}>
-          <div className="container">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-              <Link to={"/"} className="navbar-brand">
-                HOME
-              </Link>
-              <div
-                className="collapse navbar-collapse"
-                id="navbarSupportedContent"
-              >
-                <ul className="navbar-nav mr-auto">
-                  <li className="nav-item">
-                    <Link to={"/signup"} className="nav-link">
-                      Sign Up
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to={"/signin"} className="nav-link">
-                      Sign In
-                    </Link>
-                  </li>
-                </ul>
+    render() {
+        return (
+            <Router>
+                <Provider store={store}>
+                    <div className="container">
+                        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                            <Link to={"/"} className="navbar-brand">
+                                HOME
+                            </Link>
+                            <div
+                                className="collapse navbar-collapse"
+                                id="navbarSupportedContent"
+                            >
+                                <ul className="navbar-nav mr-auto">
+                                    <li className="nav-item">
+                                        <Link to={"/signup"} className="nav-link">
+                                            Sign Up
+                                        </Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to={"/signin"} className="nav-link">
+                                            Sign In
+                                        </Link>
+                                    </li>
+                                </ul>
 
-                {/*  <li className="nav-item">
+                                {/*  <li className="nav-item">
                     <Link to={"/products"} className="nav-link">
                      Products
                     </Link>
@@ -58,25 +58,26 @@ class App extends Component {
                     </Link>
                   
                   </li>*/}
-                  <Header/>
+                                <Header/>
 
-              </div>
-            </nav>{" "}
-            <br />
-            <h2>Welcome to Sysco LABS POS</h2> <br />
-            <Switch>
-            <Route exact path="/signup" component={signUp} />
-              <Route exact path="/signin" component={signIn} />
-              <Route path='/orders/:userOrderId' component={Orders} />
-              <Route path="/products" component={Products} />
-              <Route path="/userOrders" component={UserOrders} />
-            </Switch>
-          </div>
-        </Provider>
-      </Router>
+                            </div>
+                        </nav>
+                        {" "}
+                        <br/>
+                        <h2>Welcome to Sysco LABS POS</h2> <br/>
+                        <Switch>
+                            <Route exact path="/signup" component={signUp}/>
+                            <Route exact path="/signin" component={signIn}/>
+                            <Route path='/orders/:userOrderId' component={Orders}/>
+                            <Route path="/products" component={Products}/>
+                            <Route path="/userOrders" component={UserOrders}/>
+                        </Switch>
+                    </div>
+                </Provider>
+            </Router>
 
-    );
-  }
+        );
+    }
 }
 
 
